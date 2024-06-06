bbb931e03a8b49f6b14cfe8ffadd83cd488f02d0
Aung Myo Oo <aungmyooo@Aungs-MacBook-Pro.local>
2024-06-06 15:40:15 +0630

    seperate server and app

--- lottie-api/src/app.ts
+++ lottie-api/src/app.ts
@@ -1,12 +1,11 @@
+// app.ts
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { graphqlUploadExpress } from 'graphql-upload-ts';
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import typeDefs from './schema/typeDefs'; 
+import prisma from './prisma';
+import typeDefs from './schema/typeDefs';
 import resolvers from './resolvers';
-
-const prisma = new PrismaClient();
 
 const schema = makeExecutableSchema({
   typeDefs,
@@ -17,21 +16,16 @@ const app = express();
 
 app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10, overrideSendResponse: false }));
 
-const startServer = async () => {
+const startApolloServer = async (app: express.Express) => {
   const server = new ApolloServer({
     schema,
     context: () => ({ prisma }),
   });
 
   await server.start();
+  server.applyMiddleware({ app, path: '/graphql' });
 
-  server.applyMiddleware({ app });
-
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
-  );
+  return { app, server };
 };
 
-startServer().catch((error) => {
-  console.error('Failed to start server', error);
-});
+export { app, startApolloServer };
---
+++ lottie-api/src/server.ts
@@ -0,0 +1,38 @@
+// server.ts
+import http from 'http';
+import { app, startApolloServer } from './app';
+import prisma from './prisma';
+import 'dotenv/config';
+
+const SERVER_PORT = process.env.SERVER_PORT || 4000;
+
+const startServer = async () => {
+  const httpServer = http.createServer(app);
+
+  const { server } = await startApolloServer(app);
+  server.applyMiddleware({ app, path: '/graphql' });
+
+  httpServer.listen(SERVER_PORT, () => {
+    console.info(`🚀 Server ready at http://localhost:${SERVER_PORT}${server.graphqlPath}`);
+  });
+
+  process.on('uncaughtException', async (err) => {
+    console.error('Uncaught Exception. Shutting down...');
+    console.error(err.name, err.message);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
+
+  process.on('unhandledRejection', async (err) => {
+    console.error('Unhandled Rejection. Shutting down...');
+    console.error(err);
+    httpServer.close(async () => {
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+  });
+};
+
+startServer().catch((error) => {
+  console.error('Failed to start server', error);
+});
